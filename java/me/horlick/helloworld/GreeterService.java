cc6abe59a1276c9cea04c9261c90310e88e25c04
Simon Horlick <simonhorlick@gmail.com>
2016-12-11 22:47:50 +0800

    Implement the Greeter rpc logic

--- java/me/horlick/helloworld/GreeterService.java
+++ java/me/horlick/helloworld/GreeterService.java
@@ -1,3 +1,14 @@
 package me.horlick.helloworld;
 
-class GreeterService extends GreeterGrpc.GreeterImplBase {}
+import io.grpc.stub.StreamObserver;
+
+// A GreeterService implements the Greeter rpc interface by echoing back a
+// greeting in English to the supplied name.
+class GreeterService extends GreeterGrpc.GreeterImplBase {
+  @Override
+  public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+    responseObserver.onNext(
+        HelloReply.newBuilder().setMessage(String.format("hello, %s!", request.getName())).build());
+    responseObserver.onCompleted();
+  }
+}
