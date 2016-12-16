04222ef7bb8fda9bd4ffecd2889e6b7eaca869f9
Simon Horlick <simonhorlick@gmail.com>
2016-12-16 17:35:29 +0800

    Allow CORS in grpc-gateway

--- go/server.go
+++ go/server.go
@@ -2,6 +2,7 @@ package main
 import (
  "flag"
  "net/http"
+ "strings"
 
  "github.com/golang/glog"
  "golang.org/x/net/context"
@@ -12,8 +13,32 @@ import (
 )
 
 var (
- echoEndpoint = flag.String("echo_endpoint", "localhost:9090", "endpoint of Greeter")
+ echoEndpoint = flag.String("echo_endpoint", "localhost:50051", "endpoint of Greeter")
 )
+
+// allowCORS allows Cross Origin Resoruce Sharing from any origin.
+// Don't do this without consideration in production systems.
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	glog.Infof("preflight request for %s", r.URL.Path)
+	return
+}
 
 func run() error {
  ctx := context.Background()
@@ -27,7 +52,7 @@ func run() error {
    return err
  }
 
- http.ListenAndServe(":8080", mux)
+ http.ListenAndServe(":50052", allowCORS(mux))
  return nil
 }
 
