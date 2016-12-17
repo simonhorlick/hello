580e67723406c48cb4b0501319965adfcbaa25f3
Simon Horlick <simonhorlick@gmail.com>
2016-12-17 13:45:13 +0800

    web: Add basic styling

--- web/helloworld/app_module.js
+++ web/helloworld/app_module.js
@@ -3,5 +3,6 @@ goog.require('me.horlick.hello.names.Module')
 
 me.horlick.hello.app = angular.module('me.horlick.hello.app', [
   'ngRoute',
+  'ngMaterial',
   me.horlick.hello.names.Module.name,
 ]);
