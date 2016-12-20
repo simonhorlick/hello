65908ec9804f291e285ad4e18fec9b828baadb01
Simon Horlick <simonhorlick@gmail.com>
2016-12-20 13:55:22 +0800

    Add initial db deployment configuration

---
+++ infra/db/config.sh
@@ -0,0 +1,2 @@
+vitess_image="vitess/lite:v2.1.0-alpha.1"
+backup_flags="-backup_storage_implementation gcs -gcs_backup_storage_bucket 'sh-vt-backup'"
---
+++ infra/schema/greetings.sql
@@ -0,0 +1,6 @@
+CREATE TABLE greetings (
+  greeting_id BIGINT AUTO_INCREMENT,
+  name TEXT,
+  time_ns BIGINT,
+  PRIMARY KEY (greeting_id)
+)
