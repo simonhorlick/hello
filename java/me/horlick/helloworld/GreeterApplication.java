aaf104b9b084ef0ef95783a894b3c67e4e36a5b2
Simon Horlick <simonhorlick@gmail.com>
2016-12-21 16:05:40 +0800

    Add deployment for greeter server

--- java/me/horlick/helloworld/GreeterApplication.java
+++ java/me/horlick/helloworld/GreeterApplication.java
@@ -17,7 +17,7 @@ public class GreeterApplication {
 
       // Connect to the db.
       GreetingsRepositoryMySQL db =
-          GreetingsRepositoryMySQL.create(HostAndPort.fromString("vtgate-asiaeast1c:15991"));
+          GreetingsRepositoryMySQL.create(HostAndPort.fromString(args[0]));
       GreeterServer server = new GreeterServer(db);
 
       // We're ready so start the RPC server and block forever.
